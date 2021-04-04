7ba62c5b6da73e502b54c275457da48d09b6ce77
Chetan Gowda <58138652+Ch3tanGowda@users.noreply.github.com>
2021-04-05 00:08:27 +0530

    Update script.js

--- Attributes/script.js
+++ Attributes/script.js
@@ -11,4 +11,97 @@ $(document).ready(function() {
     });
   });
 });
+function myFunction() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("tk-show") == -1) {
+    x.className += " tk-show";
+  } else { 
+    x.className = x.className.replace(" tk-show", "");
+  }
+}
 
+
+var modal = document.getElementById('dataModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+var slideIndex = 1;
+  showDivs(slideIndex);
+  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
+  }
+ 
+$(window).on("load",function(){
+$(".loader-wrapper").fadeOut("slow");
+      });
+
+     
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
+appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
+
+const callback = function(entries) {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("is-visible");
+  });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".show-on-scroll");
+targets.forEach(function(target) {
+  observer.observe(target);
+});
